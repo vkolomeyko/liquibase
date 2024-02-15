f07ab482cc2351d938602271f4e575c59e5c7c4b
Steven Massaro <steven.massaro.web@gmail.com>
2024-02-15 10:43:20 -0600

    upload bundle zip to s3

--- liquibase-cli/src/main/java/liquibase/integration/commandline/LiquibaseCommandLine.java
+++ liquibase-cli/src/main/java/liquibase/integration/commandline/LiquibaseCommandLine.java
@@ -1,5 +1,12 @@
 package liquibase.integration.commandline;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import liquibase.GlobalConfiguration;
 import liquibase.Scope;
 import liquibase.command.CommandArgumentDefinition;
@@ -331,6 +338,8 @@ public class LiquibaseCommandLine {
                     addOtherFilesToZip(out);
 
                     out.close();
+
+                    uploadToS3(f);
                 } catch (Exception e) {
                     Scope.getCurrentScope().getLog(getClass()).severe("Failed to generate support bundle", e);
                 }
@@ -338,6 +347,37 @@ public class LiquibaseCommandLine {
             }
 
         }
+    }
+
+    private void uploadToS3(File f) {
+        Regions clientRegion = Regions.US_EAST_1;
+        String bucketName = "engineeringteam";
+        String fileObjKeyName = "project-zip/support-bundle-" + new Date().getTime() + ".zip";
+
+        try {
+            // This code expects that you have AWS credentials set up per:
+            // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withRegion(clientRegion)
+                    .build();
+
+            // Upload a file as a new object with ContentType and title specified.
+            PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, f);
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("application/zip");
+//            metadata.addUserMetadata("title", "someTitle");
+//            request.setMetadata(metadata);
+            s3Client.putObject(request);
+        } catch (AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process
+            // it, so it returned an error response.
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
+        Scope.getCurrentScope().getUI().sendMessage("Support bundle uploaded to S3");
     }
 
     private void addOtherFilesToZip(ZipOutputStream out) {
